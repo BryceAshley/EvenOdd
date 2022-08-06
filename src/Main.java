aab106fd86a3af2f7e368557f3c58aef17ea6d8c
BryceAshley <110739176+BryceAshley@users.noreply.github.com>
2022-08-06 14:04:44 -0400

    Initial commits - even odd

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    static double salesTax = 1.05;
+
+    static int originalPrice = 27;
+    static double totalPrice = 23;
+
+    public static void main(String[] args) {
+        double totalPrice = salesTax * originalPrice;
+        System.out.println("The sales tax is 5%, and your total is " + totalPrice +" dollars");
+
+
+    // write your code here
+    }
+}
